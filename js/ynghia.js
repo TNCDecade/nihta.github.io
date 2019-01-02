b28b0a3da0479a0be926311a966995d433b2b5e7
Nihta <44351310+Nihta@users.noreply.github.com>
2019-01-02 23:20:33 +0700

    create folder js

---
+++ js/ynghia.js
@@ -0,0 +1,141 @@
+var index = 0;
+quotes = new Array();
+
+
+
+
+
+
+
+quotes[0] = "Thất bại vì ngại thành công.";
+quotes[1] = " Học, Học nữa, Học mãi mãi.";
+
+
+quotes[2] = " Có công mài sắt có ngày chai tay.";
+
+quotes[3] = " Không buồn, không chán, tán không yêu.";
+quotes[4] = " Thuê bao quý khách vừa gọi suốt đời không liên lạc được. Xin quý khách vui lòng...<br>- Xóa số<br>- Gạch tên<br>- Và...lãng quên tất cả...😑 ";
+quotes[5] = "  Học lắm thành trâu <br>Học lâu thành bò. ";
+quotes[6] = "  Hãy sống là chính mình, bình thường nhưng không tầm thường.";
+quotes[7] = "Quá khứ có đẹp đến đâu cũng không bao giờ quay lại. <br>Cũng như em vậy chỉ là thương hại chứ không bao giờ thương yêu.hi";
+quotes[8] = " Thức ăn nghĩa là Còn…thức…còn…ăn ";
+quotes[9] = "  Hạ thấp người khác không nâng cao được giá trị của bạn. ";
+quotes[10] = "  Thờ ơ với cuộc sống, chống đối với cuộc đời.";
+quotes[11] = "  Đời hài…. Ai cũng sắm cho mình 1 vai<br>Không làm nai thì cũng làm chó dại. ";
+
+quotes[12] = "  Có công ăn vặt có ngày lên cân. ";
+quotes[13] = "  Người bi quan luôn nhìn thấy khó khăn trong cơ hội<br>Người lạc quan luôn nhìn thấy cơ hội trong khó khăn. ";
+quotes[14] = "  1+1 có thể =2. Nhưng giả nai ngây thơ thì con trai không có thể. ";
+quotes[15] = "  Trí tuệ phát triển trong im lặng, còn tính cách trưởng thành qua bão táp. ";
+quotes[16] = "  Tính anh vốn tính thật thà<br>Không yêu em trả hết quà dùm anh. ";
+quotes[17] = "  Cuộc đời là sóng gió, thịt chó là muôn năm. ";
+quotes[18] = "  Không vô học thì thành thằng vô học<br>Vô học rồi thì học cũng không vô<br>Học không vô thì cần gì vô học<br>Vô không học thà vô học còn hơn. ";
+quotes[19] = "  Tuổi học trò không yêu là tốt.<br>Yêu vào rồi cái dốt lòi ra ...<br>Ngồi học chỉ nghĩ đến người ta <br>Bài kiểm toàn là 3 với 4 😭 ";
+quotes[20] = "  Học nhiều hiểu được bảo nhiêu<br>Vừa yêu vừa học mới siêu điêu luyện. ";
+quotes[21] = "  Tiên học lễ, hậu học thêm, đêm đêm học kèm.. ";
+quotes[22] = "  Hỡi thế gian tiền là chi<br>Mà khiến đôi lứa chia ly chẳng thành. ";
+quotes[23] = "  Lời chia tay anh nói hay như hát<br>Em quay lại em tát đẹp như phim. ";
+
+quotes[24] = "  Khi tình yêu hết hạn... thì sự khốn nạn lên ngôi. ";
+quotes[25] = "  Em sẽ viết tên anh ngàn lần lên cát <br> Để sóng ngàn lần tát vỡ mặt anh ra. ";
+quotes[26] = "  Không có gì làm cho Học sinh thân thiết... bằng những tiết kiểm tra. ";
+quotes[27] = "  Chưa học xong chưa đi ngủ <br> Chưa học đủ chưa onl face. ";
+quotes[28] = "  Thầy tao nói chuyện học hành là chuyện cả đời nha mày.<br>Bởi vậy, bữa nay không học thì bữa khác học. Chuyện cả đời mà. ";
+quotes[29] = "  Học hành như cá kho tiêu <br> Kho nhiều thì mặn học nhiều thì ngu. ";
+quotes[30] = " Học chính trị ... nên trị là chính.  ";
+quotes[31] = " Yêu là chuyện nhỏ, học là chuyện lớn. Chuyện nhỏ không làm sao làm chuyện lớn được.  ";
+quotes[32] = " Dốt đến đâu, học lâu cũng thấm.  ";
+quotes[33] = " Nếu không có học sinh thì tất cả giáo viên đều mất dạy !  ";
+quotes[34] = " Người ta thường nói cuộc đời ngắn ngủi nhưng nó chính là thứ ta sở hữu lâu nhất!  ";
+quotes[35] = "  Đường lâu không đi. Đường đầy cỏ dại. <br> Người không qua lại. Người thành người dưng. ";
+quotes[36] = "  Cuộc sống phồn hoa dễ làm ta xa đọa. ";
+quotes[37] = "  Kinh nghiệm không phải là thứ để rút mãi. ";
+quotes[38] = "  Sống thì phải có mục đích, <br> Mục đích ấy đừng quá khó cũng đừng quá dễ vì, <br> chúng ta đừng quá ràng buộc bản thân và cũng đừng quá dễ dãi với nó. ";
+quotes[39] = "  Cuộc đời là thế... để tâm đến bản thân thì đời là một con đường!!!<br> Để tâm đến người khác thì đời là một mê cung... ";
+quotes[40] = "  Muốn nhìn thấy cầu vồng phải chịu đựng cơn mưa. ";
+quotes[41] = " Cuộc đời không có gì là miễn phí...  ";
+
+quotes[42] = "  Con người luôn không hài lòng với những gì mình đang có. <br> Và luôn hối tiếc với những gì mình đã mất. ";
+quotes[43] = "  Ai cũng phải mang gánh nặng trên vai.<br> Nhưng không phải ai cũng biết biến nó thành sức mạnh. ";
+quotes[44] = " Người thích và quý bạn thì ít thôi. <br> Chứ người ghét và không quan tâm tới bạn thì nhiều lắm.  ";
+quotes[45] = " Muốn hoa thơm thì đừng nên chọn sắc. <br> Chọn cuộc đời chứ ai chọn cuộc chơi.  ";
+quotes[46] = "  Nếu muốn đi nhanh hãy đi một mình. <br> Nhưng nếu muốn đi xa hãy đi cùng nhau. ";
+quotes[47] = " Sống ở đời này các phải biết ...<br> Nhẫn nhục với kẻ mạnh ... Nhẫn nhịn với kẻ liều ...<br> Nhẫn tâm với kẻ yếu   ";
+quotes[48] = "  Cuộc sống nó không giống như cuộc đời, vì cuộc đời nó tơi bời hơn cuộc sống. ";
+quotes[49] = "  Hãy sống như chưa từng tồn tại.. ";
+quotes[50] = "  Khi bạn yêu đời thì bạn có thể tha thứ cho người đời rất nhiều chuyện. ";
+quotes[51] = "  Không có hoàn cảnh nào tuyệt vọng, chỉ có người tuyệt vọng vì hoàn cảnh. ";
+quotes[52] = "  Sống sao cho người ta nể, đừng kể lể cho người ta khinh. ";
+quotes[53] = " Cuộc đời là những chuyến đi, gặp nhau thì ít chia li thì nhiều.  ";
+
+quotes[54] = "  Con người càng lúc càng đông. <br> Thạch Sanh thì ít , Lý thông thì nhiều . ";
+quotes[55] = "  Sự lợi hại thực sự không phải là bạn quen biết bao nhiêu người, <br> Mà là vào lúc bạn gặp hoạn nạn, có bao nhiêu người quen biết bạn. ";
+quotes[56] = "  Thật dễ dàng để làm quen với một người xa lạ.<br> Nhưng thật khó để quên đi một người xa lạ đã từng quen. ";
+quotes[57] = "  Bi kịch của cuộc đời là ở chỗ ta già đi quá nhanh mà khôn ngoan thì quá trễ... ";
+quotes[58] = " Dẫu biết rằng đường đời nhiều sỏi đá  <br> Chỉ mong rằng vấp ngã vẫn còn răng.  ";
+quotes[59] = "  Trên đời, có nhiều người lạ lắm. <br>Họ chỉ thấy lỗi sai của người khác mà không nhìn thấy đống rác trên đầu mình! ";
+
+
+quotes[   60   ]  = " Chọn bạn mà chơi, chọn nơi mà ở. ";
+quotes[  61    ] = " Khoa học đã chứng minh, bệnh thần kinh lây rất nhanh qua tình bạn.  ";
+quotes[   62   ] = " Anh em bốn bể là nhà<br> Tuy rằng khác họ vẫn là anh em 😎 .";
+quotes[   63   ] = " Chơi hoa thích nhất hoa đào<br> Chơi bạn ghét nhất bạn nào tự cao. ";
+quotes[   64   ]  = "Bạn bè là một thứ gì đó rất xa xỉ. <br> Quen biết cả tỉ, mà tri kỉ được mấy người.    ";
+quotes[  65    ] = " Kẻ thù ngày hôm nay có thể là chiến hữu của ngày mai.    ";
+quotes[  66    ] = "   Trên đời có 3 loại bạn bè: <br> Quen biết, thân thiết và không cần thiết.  ";
+quotes[   67   ]  = "    Mày có tiền, mày có quyền lắm bạn. <br> Tao ít bạn nhưng hoạn nạn có nhau.";
+quotes[ 68     ] = "   Bạn thân là đứa sẽ cười khi bạn ngã rồi nhẹ nhàng nâng đỡ bạn lên và rồi đẩy bạn ngã để cười tiếp.  ";
+quotes[  69    ] = "   Người hay chủ động thanh toán tiền, không phải là ngu ngốc lắm tiền mà là người ta coi trọng bạn bè hơn tiền bạc.  ";
+quotes[  70    ] = "Nếu muốn có bạn tốt thì trước hết hãy trở thành bạn tốt.     ";
+quotes[  71    ]  = " Sự phê bình của người bạn còn tốt hơn lời nịnh nọt của kẻ thù.   ";
+quotes[  72    ] = "  Đừng add friend khi chưa hiểu nhau <br> Đừng unfriend chỉ vì hiểu nhầm. ";
+quotes[ 73     ] = "   Cha mẹ khó nhọc nuôi ta lớn.<br>Bạn bè chơi “xấu” dạy ta khôn.  ";
+quotes[   74   ] = "   Một người bạn tốt sẽ nhìn thấy giọt nước mắt đầu tiên, lau đi giọt nước mắt thứ hai và ngăn lại giọt nước mắt thứ ba.  ";
+quotes[   75   ]  = " Thành thật có thể không mang lại nhiều bạn bè nhưng nó luôn mang lại những người bạn đúng nghĩa.   ";
+quotes[   76   ] = "   Chuyện ăn ý nhất mà tôi với bạn cùng bàn của tôi đã làm chính là, tới tận bây giờ, tôi không liên lạc với nó, nó cũng không liên lạc với tôi.  ";
+quotes[   77   ] = "   Chúng ta là những người bạn thân, thân theo kiểu thân ai người ấy lo.  ";
+quotes[  78    ] = "    Bạn thân là đứa có đánh chết cũng không khen bạn một câu, nhưng nó luôn nói tốt về bạn sau lưng với đứa khác. ";
+quotes[   79   ]  = "  Tình bạn làm niềm vui tăng lên gấp đôi và nỗi đau khổ giảm đi một nửa.  ";
+quotes[ 80     ] = "  Khi thân nhau thì kể nhau nghe những câu chuyện bí mật...<br> Khi trở mặt thì khai quật những bí mật của nhau.<br> 😂😂😂😂😂  ";
+quotes[  81    ] = "  Một người bạn thật sự là người bước vào cuộc sống của bạn khi cả thế giới đã bước ra.   ";
+quotes[ 82     ] = "   Không phải cứ thân nhau là bạn.<br> Ở đời còn rất nhiều đứa khốn nạn gắn nhãn bạn thân.  ";
+quotes[  83    ]  = "   Nếu là bạn xin mãi mãi là bạn , đừng như sông lúc cạn lúc đầy. ";
+quotes[  84    ] = "    Thông minh là tốt nhưng đừng giấu dốt rồi đột ngột thông minh. ";
+quotes[  85    ] = "   Bạn không hề ngu tí nào chẳng qua là mọi người xung quanh bạn quá thông minh mà thôi.  ";
+quotes[ 86     ] = "    Nếu không muốn trở thành một thằng ngốc trong mắt người khác thì lúc đầu đừng tỏ nên quá thông minh. ";
+quotes[  87    ]  = "  Không phải chú dốt... mà chỉ vì mẹ chú quên bỏ i-ốt vào canh.  ";
+quotes[    88 ] = "  Thằng ngốc tự cho mình là thông minh, còn người thông minh biết mình là thằng ngốc.   ";
+quotes[  89    ] = "  Có 3 thứ ngu dốt: không biết điều gì mình cần biết, không rành những gì mình biết và biết điều gì mình không cần biết.   ";
+quotes[  90    ] = "    Ngu không phải cái tội mà cái tội là không biết mình ngu. ";
+quotes[  91    ] = "  Tui ghét những người ăn trộm ý tưởng của tui ... trước khi tui nghĩ ra nó.   ";
+quotes[   92   ] = "   Tôi có thể làm tất cả vì tiền thậm chí đó là.....những việc tốt.  ";
+quotes[ 93     ] = "  Mỗi ngày thức dậy, tôi nhìn vào danh sách những người giàu nhất thế giới. <br>Nếu không có tên mình ở đó, tôi sẽ dậy và làm việc.   ";
+quotes[ 94     ] = "   Tôi rất biết ơn những người đã nói không với tôi.<br> Nhờ vậy mà tôi biết cách tự mình giải quyết sự việc.  ";
+quotes[   95   ] = "    Đừng nhầm lẫn tính cách tôi và thái độ tôi. <br> cách của tôi nói lên con người tôi, còn thái độ của tôi tuỳ thuộc vào con người bạn. ";
+quotes[  96    ] = "    Tôi không phải đồ chơi, để người chơi rồi.<br>Tôi không phải công thức, đề người cộng rồi chia. ";
+quotes[ 97     ] = "   Ngoài đời, Công Lý là diễn viên hài.  ";
+quotes[  98    ] = "   Kẻ chung tình thì hay thất bại.<br>Kẻ lừa tình thì lại thành công.  ";
+quotes[   99   ] = " Giờ mới biết, lúc lớp đoàn kết nhất là khi...... Kiểm tra.    ";
+quotes[    100  ] = " Khi vào phòng thi phải đưa ra 2 định lí cần nhớ:<br> Pí-ta-ngó và Khó-ta- liếc.<br>😂😂😂 ";
+
+
+
+
+
+
+
+
+
+
+
+index = Math.random()*(quotes.length);
+index = Math.floor(index);
+function rotator() {
+
+index = Math.random()*(quotes.length);
+index = Math.floor(index);
+if (index == quotes.length) {index = 0;}
+document.getElementById("rotation").innerHTML = quotes[index];
+
+setTimeout("rotator();",5000);
+}
